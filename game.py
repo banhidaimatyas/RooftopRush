77427e531ad3f7a2bb5abaa029b945f0dcd8cfa4
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2024-02-16 08:56:59 +0100

    game.py alap

--- game.py
+++ game.py
@@ -2,4 +2,10 @@ import pygame
 
 class Game:
     def __init__(self) -> None:
-        pass
+        pygame.init()
+        
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
