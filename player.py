16b9ef05d06eaabd53898ce52f2ed644ff8983fd
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2024-02-25 18:22:50 +0100

    adattípusok

--- player.py
+++ player.py
@@ -1,34 +1,30 @@
 import pygame
 
+
 class Player:
-    def __init__(self, pos):
+    def __init__(self, pos: tuple[int]):
         super().__init__()
-        self.image = pygame.image.load('')
-        self.rect = self.image.get_rect(topright = pos)
-        self.direction = pygame.math.Vector2(0,0)
-        self.speed = 7
-        self.gravity = 1
-        self.jump_speed = -10
-        self.on_ground = True
+        self.image = pygame.image.load("")
+        self.rect: pygame.Rect = self.image.get_rect(topright=pos)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed: int = 7
+        self.gravity_value: int = 1
+        self.jump_speed: int = -10
+        self.on_ground: bool = True
 
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0
-        
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        else:
-            self.direction.x = 0
-
-        if keys[pygame.K_SPACE] and self.on_ground:
+        if keys[pygame.K_UP] and self.on_ground:
             self.on_ground = False
             self.jump()
+        if keys[pygame.K_DOWN] and self.on_ground:
+            self.slide()
 
+    def slide(self):
+        pass
+        
     def gravity(self):
-        self.direction.y += self.gravity
+        self.direction.y += self.gravity_value
         self.rect.y += self.direction.y
 
     def jump(self):
