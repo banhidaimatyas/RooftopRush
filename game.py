9b9c04f4c82186f8d5eecbfd938cd365ff0778f2
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2024-02-25 18:26:55 +0100

    png fájlok

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game:
     def bg(self) -> None:
         self.screen: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
         self.bg_surf = pygame.image.load(
-            "Img/Map/background.jpg"
+            "Img/Map/background.png"
         ).convert_alpha()
         self.bg_surf = pygame.transform.rotozoom(self.bg_surf, 0, 1.5)
         self.bg_rect: pygame.rect.Rect = self.bg_surf.get_rect(topleft=(0, 0))
