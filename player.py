568b477997b91c4b818174f4a3b370f22952af36
Németh Zalán <Nemeth.Zalan@students.jedlik.eu>
2024-02-29 13:20:33 +0100

    folytatás

--- player.py
+++ player.py
@@ -6,15 +6,15 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, x: int, y: int):
         pygame.sprite.Sprite.__init__(self)
-        self.animations = {'Idle': [], 'Running': []}
+        self.animations = {'Running': []}
         # self.image = pygame.image.load(
         #     "Img/Character/PNG Sequences/Idle/0_Fallen_Angels_Idle_000.png"
         # ).convert_alpha()
-        self.image = self.animations['idle'][self.frame_index]
+        self.image = self.animations['Running'][1]
+        self.import_character_assets()
         self.image = pygame.transform.rotozoom(self.image, 0, 0.1)
-        self.frame_index = 0
+        self.frame_index: int = 0
         self.animation_speed = 0.15
-        self.image = self.animations['idle'][self.frame_index]
         self.rect: pygame.Rect = self.image.get_rect(topright=(x, y))
         self.gravity_value: int = 1
         self.jump_speed: int = -10
@@ -23,12 +23,22 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed: int = -16
         self.dy: int = 0
 
-        self.status = "Idle"
+        self.status = "Running"
+
+    def animate(self):
+        animation = self.animations[self.status]
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        self.image = animation[int(self.frame_index)]
+
+    def get_status(self):
+        self.status = 'Running'
 
 
     def import_character_assets(self):
-        character_path = "Img/Character/PNG Sequences"
-        for animation in self.animation.keys():
+        character_path = "Img/Character/PNG Sequences/"
+        for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
 
@@ -48,5 +58,9 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         super().update(*args, **kwargs)
-        self.apply_gravity()
         self.input()
+        self.get_status()
+        self.animate()
+        self.apply_gravity()
+        
+        
