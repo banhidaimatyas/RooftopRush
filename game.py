b5914f0ca8d24f26db3d6c64cd797a404fc496cb
kallaigabor06 <kallai.gabor@students.jedlik.eu>
2024-03-08 08:32:32 +0100

    készen van

--- game.py
+++ game.py
@@ -15,8 +15,13 @@ class Game:
         self.clock = pygame.time.Clock()
         self.bg()
         
+        
         self.characters: pygame.sprite.GroupSingle[Any] = pygame.sprite.GroupSingle()
         self.player: Player = Player(WIDTH // 2, HEIGHT // 2)
+        self.game_font = pygame.font.Font("Img/Font/tarrget.ttf", 30)
+        self.font_colour = (255, 255, 255)
+
+        self.menu()
 
     def bg(self) -> None:
         self.screen: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -29,16 +34,15 @@ class Game:
         self.menu_surf = pygame.image.load("Img/Map/menu.jpg").convert_alpha()
         self.menu_surf = pygame.transform.scale(self.menu_surf, (900, 600))
         self.menu_rect: pygame.rect.Rect = self.bg_surf.get_rect(topleft=(0, 0))
-        self.game_font = pygame.font.SysFont('arial', 200, bold = True)
-        self.run_surf = self.game_font.render("Nyomd meg a SPACE-t az indításhoz!", True, 255,255,255)
-        self.run_rect = self.run_surf.get
+        self.run_surf = self.game_font.render("Nyomd meg a SPACE-t az indításhoz!", True, self.font_colour)
+        self.run_rect = self.run_surf.get_rect(center=(WIDTH / 2, HEIGHT / 2))
 
 
     
 
     def run(self) -> None:
         running: bool = True
-        game_active: bool = True
+        game_active: bool = False
 
         if running is False:
             pygame.quit()
@@ -56,6 +60,11 @@ class Game:
             
             else:
                 self.screen.blit(self.menu_surf, self.menu_rect)
+                self.screen.blit(self.run_surf, self.run_rect)
+
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    game_active = True
 
             
             
