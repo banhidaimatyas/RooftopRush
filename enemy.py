97e28afb8dda709ade06bcbb179e1b400a5be373
Németh Zalán <Nemeth.Zalan@students.jedlik.eu>
2024-03-13 13:14:40 +0100

    enemy-k inicializálása

---
+++ enemy.py
@@ -0,0 +1,28 @@
+import pygame
+from typing import Any
+from settings import GAME_SPEED
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, enemy_type: str, x: int, y: int):
+        pygame.sprite.Sprite.__init__(self)
+
+        if enemy_type == "1":
+            self.image: pygame.Surface = pygame.image.load(
+                ""
+            ).convert_alpha()
+        elif enemy_type == "2":
+            self.image: pygame.Surface = pygame.image.load(
+                ""
+            ).convert_alpha()
+        
+        self.rect: pygame.Rect = pygame.Rect(x, y, 900, 450)
+
+    def destroy(self):
+        if self.rect.right <= -1:
+            self.kill()
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        super().update(*args, **kwargs)
+
+        self.rect.x -= GAME_SPEED
+        self.destroy()
