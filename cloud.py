e48867a5a9654375b8776dfa7c2532f15f3d8815
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2024-04-05 08:16:10 +0200

    kép, rect, surface

--- cloud.py
+++ cloud.py
@@ -6,6 +6,9 @@ class Cloud(pygame.sprite.Sprite):
     def __init__(self, x: int, y: int):
         pygame.sprite.Sprite.__init__(self)
         
+
+        self.image: pygame.Surface = pygame.image.load("Img/Map/cloud.png")
+        self.rect: pygame.Rect = pygame.Rect(x, y, 321, 306)
         
         
     def update(self, *args: Any, **kwargs: Any) -> None:
